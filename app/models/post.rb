1f2c291a4013bd187696fbe262ca10c968cba6f9
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-08 04:02:45 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, :content, :category, presence: true
   validates :content, length: { minimum: 100 }
-  validates_inclusion_of :category, :in => [fiction, non-fiction]
+  validates_inclusion_of :category, :in => %w(fiction, non-fiction)
 end
