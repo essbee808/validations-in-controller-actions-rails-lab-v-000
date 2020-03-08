f5ce64e546788b67ca44e5323f7585056fc9b1c1
Sheena Bonilla <sheena.myb@gmail.com>
2020-03-08 04:02:02 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, :content, :category presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, presence: true 
+  validates :category, presence: true
 end
